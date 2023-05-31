4c128b44a7b44d80cee6c63ec7d6c88de7991385
s4ngwoo <s4ngwoo.lee@gmail.com>
2023-05-31 13:58:46 +0900

    add ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [43.200.102.127]
 
 
 # Application definition
